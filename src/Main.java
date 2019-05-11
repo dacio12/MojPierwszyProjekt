2900bfd59602cc4c70459878b3b0048a6d2b092e
dacio12 <dawidscibor1@gmail.com>
2019-05-11 14:09:06 +0200

    NewCommit2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Witaj swiecieee!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+        }
     }
 }
